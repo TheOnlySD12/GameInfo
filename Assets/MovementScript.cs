f00dd33372b5adb535710ee176435def23fca95b
Burrito910 <103515044+Burrito910@users.noreply.github.com>
2022-10-12 15:03:15 +0300

    am adaugat groundcheck, etc

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -9,7 +9,9 @@ public class MovementScript : MonoBehaviour
     
     public float runSpeed;
     public float jumpSpeed;
-    //public bool isGrounded = true;
+    
+    
+    public bool isGrounded = true;
 
     public Rigidbody2D rb;
     
@@ -18,12 +20,22 @@ public class MovementScript : MonoBehaviour
         //movement
         
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed, rb.velocity.y) ;
-        if (Input.GetButton("Jump") /*&& isGrounded*/)
+        if (Input.GetButton("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            //isGrounded = false;
+            isGrounded = false;
+            
         }
         //DE ADAUGAT GROUNDCHECK:
         //pentru performanta ca sa nu dea check la fiecare frame, verifican daca are ceva sub el doar cand atinge alt collider.
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit");
+        if (Physics2D.OverlapBox(new Vector2(0, transform.localScale.y/2-transform.localScale.y), new Vector2(transform.localScale.x, transform.localScale.y*100), 0) == true)
+        {
+            isGrounded = true;
+            Debug.Log("grounded");
+        }
     }
 }
