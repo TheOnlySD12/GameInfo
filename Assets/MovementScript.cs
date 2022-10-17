cda5c38bfb1219306a5b12d17c08f235505d1b28
Burrito910 <103515044+Burrito910@users.noreply.github.com>
2022-10-17 15:09:53 +0300

    achim 17 octombrie.

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -31,16 +31,18 @@ public class MovementScript : MonoBehaviour
         {
             //col.size.y = col.size.y / 2;
         }
+        
+        if (Physics2D.OverlapBox(new Vector2(0, 0 - transform.localScale.y / 2), new Vector2(transform.localScale.x / 100, transform.localScale.y / 100), 0, 8) == true)
+        {
+            isGrounded = true;
+            Debug.Log("grounded");
+        }
+
         //DE ADAUGAT CROUCH
         //Ar trebui sa faca collider-ul sa isi injumatateasca marimea y.
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("hit");
-        if (Physics2D.OverlapBox(new Vector2(0, transform.localScale.y/2-transform.localScale.y), new Vector2(transform.localScale.x, transform.localScale.y*100), 0) == true)
-        {
-            isGrounded = true;
-            Debug.Log("grounded");
-        }
+        
     }
 }
