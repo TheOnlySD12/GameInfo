52104a4fedfe1bbb0cd3ab8c98c33e4f0740fd27
danutz420 <103685069+danutz420@users.noreply.github.com>
2022-10-19 14:52:34 +0300

    Movement cum trebe(nou)
    PENTRU FIECARE PLATFORMA PE CARE STAI TREBE PUS TAGUL "GROUND'"PE EA

---
+++ Assets/Movement.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    private Rigidbody2D body;
+    [SerializeField] private float jumpspeed = 5.0f;
+
+    private bool isjumping;
+
+    bool facingRight;
+
+    private Vector3 respawnPoint;
+    public GameObject FallDetector;
+
+
+    void Start()
+    {
+        respawnPoint = transform.position;
+    }
+
+
+
+
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); //mers orizontal
+
+        if (Input.GetButtonDown("Jump") && !isjumping)  //sarit
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpspeed);    //sarit
+            isjumping = true;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0 && !facingRight)
+        {         // se intoarce stanaga/ dreapta
+            Flip();
+
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0 && facingRight)        // se intoarce stanaga/ dreapta
+        {
+            Flip();
+        }
+
+        FallDetector.transform.position = new Vector2(transform.position.x, transform.position.y);
+
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FallDetector")
+        {
+            transform.position = respawnPoint;
+
+        }
+        if (collision.tag == "NextLevelPlace")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {     //check daca e pe pamant
+            isjumping = false;
+
+        }
+    }
+
+    void Flip()
+    {
+
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;    //flip stanga dreapta
+    }
+
+
+}
