aab5af22e394709e8df60a3a2fb557a204abee78
TheOnlySD12 <103511260+TheOnlySD12@users.noreply.github.com>
2022-10-20 14:31:54 +0300

    chestii

--- Assets/Scripts/SavesMenu.cs
+++ Assets/Scripts/SavesMenu.cs
@@ -8,4 +8,7 @@ public class SavesMenu : MonoBehaviour
     public void PlayGame () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame () {
+        Application.Quit();
+    }
 }
