c2747e4d8e14a810466277493af9311b05b40639
TheOnlySD12 <103511260+TheOnlySD12@users.noreply.github.com>
2022-11-02 15:12:37 +0200

    SavesMenu chestii meniu

--- Assets/Scripts/SavesMenu.cs
+++ Assets/Scripts/SavesMenu.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
-public class SavesMenu : MonoBehaviour 
-{
+public class SavesMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
     public void PlayGame () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -12,8 +19,43 @@ public class SavesMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void Back2Menu()
-    {
+    public void Back2Menu() {
         SceneManager.LoadScene("SavesMenu");
+    }
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
