abf59b89598a725226ebcadfda89d7f9dfb50b05
TheOnlySD12 <103511260+TheOnlySD12@users.noreply.github.com>
2022-11-17 14:03:01 +0200

    Map si alte chestii

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,16 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Map : MonoBehaviour
 {
-    public void BackAScene()
-    {
+    public void BackAScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
-    public void NextScene()
-    {
+    public void NextScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Application.Quit();
+    }
+
+    public void LoadPlayer() {
+        PlayerData data = SaveSystem.LoadPlayer();
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
     }
 }
