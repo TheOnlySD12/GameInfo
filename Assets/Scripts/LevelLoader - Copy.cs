6d6e06398b2ce56c4de1f2f7b3beb5ac24d2b5a7
RaulPop2008 <115152357+RaulPop2008@users.noreply.github.com>
2022-12-15 14:11:39 +0200

    raul

--- Assets/Scripts/LevelLoader - Copy.cs
+++ Assets/Scripts/LevelLoader - Copy.cs
@@ -7,14 +7,14 @@ public class LevelLoader : MonoBehaviour
     
     public void LoadLevel (int sceneIndex)
     {
-        
+        StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
     IEnumerator LoadAsynchronously (int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        while (operation.isDone)
+        while (!operation.isDone)
         {
             Debug.Log(operation.progress);
 
