332e8c445df3fc1266bbbb63518edd229164a104
Burrito910 <103515044+Burrito910@users.noreply.github.com>
2023-01-12 14:06:04 +0200

    astronaut a invatat sa mearga!!

--- Assets/Scripts/Astronaut.cs
+++ Assets/Scripts/Astronaut.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -36,37 +37,46 @@ public class Astronaut : MonoBehaviour
         return Physics2D.OverlapCircle(forwardGroundCheck.position, 0f, groundLayer);
     }
 
-    private float randomX;
     private float moveTimer;
+    private float moveDuration;
+    private bool moveDirection;// 1 = positive x, -1 = negative x
     private void Update()
     {
         moveTimer -= Time.deltaTime;
         
-        if (moveTimer < 0)
+        if (moveTimer > 0)
         {
-            moveTimer = (Random.value + 0.5f) * 1.5f;
-            randomX = (Random.value - 0.5f) * 50;
-
-            Walk(randomX);
+            body.velocity = new Vector2(3 * ((Convert.ToInt32(moveDirection) - 0.5f) * 2), body.velocity.y);  // convert transforma bool in int 0 sau 1
         }
         
+        if (moveTimer <= 0)
+        {
+            body.velocity = new Vector2(0, body.velocity.y);
+            
+        }
+        
+        if(moveTimer <= -1)
+        {
+            moveTimer = Random.value * 2 + 1.5f;
+            moveDirection = (Random.value - 0.5f) > 0;
+        }
         
     }
 
     
 
-    private float moveDuration;
-    void Walk(float x)
+    
+    /*void Walk(float x)
     {
-        moveDuration = x;
-        
-        while(moveDuration > 0)
+        moveDuration = Mathf.Abs(x);
+        Debug.Log("moveDuration = " + moveDuration);
+        while (moveDuration > 0)
         {
             body.velocity = Vector2.right * x;
             moveDuration -= Time.deltaTime;
         }
         //body.velocity = Vector2.zero;
-        Debug.Log("Astronaut moved");
-    }
+        Debug.Log("Astronaut moved" + body.velocity.x);
+    }*/
     
 }
